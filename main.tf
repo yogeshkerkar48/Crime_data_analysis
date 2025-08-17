367f1aa4e74641b1598163d4a578c3acfd9b8504
yogeshkerkar48 <56548865+yogeshkerkar48@users.noreply.github.com>
2025-08-17 19:51:44 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,34 @@
+resource "aws_glue_catalog_database" "etl_db" {
+  name = "crime_db123"
+}
+
+locals {
+  glue_role_arn = var.glue_role_arn
+}
+
+resource "aws_glue_job" "etl_job" {
+  name     = var.glue_job_name
+  role_arn = local.glue_role_arn
+
+  command {
+    name            = "glueetl"
+    script_location = var.script_s3_path
+    python_version  = "3"
+  }
+
+  glue_version      = "4.0"
+  number_of_workers = 2
+  worker_type       = "G.1X"
+}
+
+resource "aws_glue_crawler" "my_crawler" {
+  name          = var.glue_crawler_name
+  role          = local.glue_role_arn
+  database_name = aws_glue_catalog_database.etl_db.name
+
+  s3_target {
+    path = "s3://rawdatagroup5123123456/transformed-data_automate/"
+ }
+
+  depends_on = [aws_glue_job.etl_job]
+}
